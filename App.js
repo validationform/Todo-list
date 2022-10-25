cc36072d4c9c82b5d2cbff4a2d60145468d00155
Rashmi Bora <you@example.com>
2022-10-25 13:05:20 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,18 @@
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
+import { TodosProvider } from "./contexts/todos";
+
+function App() {
+  return (
+    <TodosProvider>
+      <div className="todoapp">
+        <Header />
+        <Main />
+        <Footer />
+      </div>
+    </TodosProvider>
+  );
+}
+
+export default App;
---
+++ index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./todo-mvc-base.css";
+import "./todo-mvc-app.css";
+import App from "./App";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
